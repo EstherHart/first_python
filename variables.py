bf40364d77ca40a8e9234b105d48f1501ab4ca43
Esther Hart <esther@blacklight.net>
2017-03-08 20:40:23 -0800

    first day

---
+++ variables.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+# String Assingment
+name = 'Esther'
+lastname = ''
+# lastname = String
+# Integer Assingment
+age = 3
+# number_of_teeth = Integer
+# List Assingment
+family = ['Esther','Sean','Heather']
+kuniyoshis = []
+# taverniers = List
+# Dictionary Assingment
+esther = { 'eyes': 'purple',
+           'hair': 'blonde',
+           'brain': 'big',
+           'poop': 'stinky' }
+
+# Print a String
+print(name)
+
+# add two numbers
+total = age + 8
+print(total)
+
+# Multiply tow numbers
+total = 7 * 8
+print(total)
+
+# divide
+total = 56 / 7
+print(total)
+
+# subtract
+total = 1234 - 7890
+print(total)
